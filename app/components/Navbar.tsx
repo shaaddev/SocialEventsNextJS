19f985b4a104c982d8022abfcf324cb01996b1f3
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-01-13 14:57:46 -0400

    fixed minor bug

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,15 +9,15 @@ export default function Navbar() {
         <nav className="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-white bg-neutral-900 border rounded-none shadow-md h-max border-neutral-950/80 bg-opacity-30 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
                 <div className="flex items-center justify-between text-blue-gray-900">
                   {user ? 
-                    <a href="/"
+                    <Link href="/"
                       className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
                       Welcome, @{username}
-                    </a>
+                    </Link>
                   : 
-                    <a href="/"
+                    <Link href="/"
                       className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
                         Welcome
-                    </a>}
+                    </Link>}
                   <div className="flex items-center gap-4">
                     <div className="hidden mr-4 lg:block">
                       <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
