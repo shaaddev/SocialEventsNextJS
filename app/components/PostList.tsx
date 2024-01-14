49c4453719e32caab09cead91cb807c73d075672
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-01-14 12:32:07 -0400

    added types

--- app/components/PostList.tsx
+++ app/components/PostList.tsx
@@ -5,6 +5,14 @@ import Link from 'next/link';
 import { HiDotsVertical } from 'react-icons/hi';
 import DeleteBtn from './DeleteBtn';
 
+type Posts = {
+    _id: string;
+    title: string;
+    caption: string;
+    location: string;
+    event_date: string;
+    created_at: string;
+}
 
 export default function PostList(){
     const [posts, setPosts] = useState([]);
@@ -20,7 +28,7 @@ export default function PostList(){
         <> 
             <h1 className='m-5'>Social Events</h1>
             <div className='container mx-auto px-1 my-10 justify-center'>
-            {posts.map((p: any) => (
+            {posts.map((p: Posts) => (
                     <div className='all_posts' key={p._id}>
                         <div className='p-5'>
                             <h3 className="post_user">@username</h3>
