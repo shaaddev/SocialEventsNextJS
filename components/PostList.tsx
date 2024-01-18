299a225c522c0554237fcb20883179183a6f6ffd
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-01-18 19:43:25 -0400

    switched to server side logic

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,6 +1,5 @@
-'use client';
 import '@/styles/post_list.css';
-import { useState, useEffect } from 'react';
+const { posts } = require('@/lib/connect');
 
 
 type Posts = {
@@ -11,21 +10,14 @@ type Posts = {
     event_date: string;
 }
 
-export default function PostList(){
-    const [posts, setPosts] = useState([]);
-
-    useEffect(() => {
-        fetch('/api/posts')
-            .then((res) => res.json())
-            .then((data) => setPosts(data))
-            .catch((err) => console.log('failed to fetch: ', err))
-    }, [])
+export default async function PostList(){
+    const post = await posts.find({});
 
     return(
         <> 
             <h1 className='m-5'>Social Events</h1>
             <div className='container mx-auto px-1 my-10 justify-center'>
-            {posts.map((p: Posts) => (
+            {post.map((p: Posts) => (
                     <div className='all_posts' key={p._id}>
                         <div className='p-5'>
                             <h3 className="post_user">@username</h3>
--- components/UserPostList.tsx
+++ components/UserPostList.tsx
@@ -1,10 +1,9 @@
-'use client';
 import '@/styles/post_list.css';
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { HiDotsVertical } from 'react-icons/hi';
 import DeleteBtn from './DeleteBtn';
-import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+const { posts } = require('@/lib/connect');
 
 type Posts = {
     _id: string;
@@ -14,25 +13,16 @@ type Posts = {
     event_date: string;
 }
 
-export default function UserPostList(){
-    const [posts, setPosts] = useState([]);
+export default async function UserPostList(){
+    const post = await posts.find({});
 
-    useEffect(() => {
-        fetch('/api/posts')
-            .then((res) => res.json())
-            .then((data) => setPosts(data))
-            .catch((err) => console.log('failed to fetch: ', err))
-    }, [])
+    const { isAuthenticated } = getKindeServerSession();
 
-    const { isAuthenticated, isLoading } = useKindeBrowserClient();
-
-    if (isLoading) return <div>Loading...</div>
-
-    return isAuthenticated ? (
+    return ( await isAuthenticated()) ? (
         <> 
             <h1 className='m-5'>Social Events</h1>
             <div className='container mx-auto px-1 my-10 justify-center'>
-            {posts.map((p: Posts) => (
+            {post.map((p: Posts) => (
                     <div className='all_posts' key={p._id}>
                         <div className='p-5'>
                             <h3 className="post_user">@username</h3>
