03b1f81294eab9da9386d13e9a8ae0e4a7df46df
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-01-27 16:19:16 -0500

    responsive update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PostList from '@/components/PostList'
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <main className="flex min-h-screen flex-col items-center justify-between px-6">
         <div>
           <PostList />
         </div>
